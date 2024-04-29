4b542b6c4dc60244aa772a16a41e07c3dc684ae0
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2024-04-29 14:00:21 -0400

    tweaks

--- Engine Files/scripts/civilization.gd
+++ Engine Files/scripts/civilization.gd
@@ -5,6 +5,7 @@ var fuel_count
 var oxygen_count
 var food_count
 var water_count
+var resources
 var building_list = []
 
 var pop_growth_multiplier
@@ -13,12 +14,13 @@ var pop_decline_multiplier
 var tick = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready(init_pop, init_fuel, init_oxygen, init_food, init_water, init_buildings, growth, decline):
+func _ready(init_pop, init_fuel, init_oxygen, init_food, init_water, init_buildings, init_resources, growth, decline):
 	self.population = init_pop
 	self.fuel_count = init_fuel
 	self.oxygen_count = init_oxygen
 	self.food_count = init_food
 	self.water_count = init_water
+	self.resources = init_resources
 	self.building_list = init_buildings
 	
 	self.pop_growth_multiplier = growth
@@ -30,7 +32,6 @@ func passive_resource_consumption():
 	self.oxygen_count -= self.population * 2.2 # eventually replace hardcoded numbers with a variable that can be tweaked
 	self.water_count -= self.population * 2.5 # im thinking that different civilizations will require different needs, an alien species can require less water than humans, just an idea though
 	self.food_count -= self.population * 2
-	self.fuel_count -= self.population * 1.5 # placeholder, i dont really know how much you want fuel to drain each cycle
 	
 
 func population_growth_decline():
