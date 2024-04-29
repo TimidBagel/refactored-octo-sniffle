15d274e310552b8c1acafd02979745b27163eab8
Iain Broomell <slaprabbit805@gmail.com>
2024-04-29 14:25:52 -0400

    added basic planet script

---
+++ Engine Files/planet.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var civilizations # list
+@export var breathable_atmosphere : bool
+@export var water_source : bool
+@export var resource_harvest_multiplier : float
+@export var temperature : String
